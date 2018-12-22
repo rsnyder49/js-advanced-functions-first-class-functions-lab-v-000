2fe0428bfc9d7a29f1147a1f77f42fda5c97f3a3
Robert Snyder <r.snyder49@gmail.com>
2018-12-22 20:46:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,3 +6,11 @@ const returnFirstTwoDrivers = function (drivers) {
 const returnLastTwoDrivers = function (drivers) {
   return drivers.slice(-2);
 };
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+ 
+function createFareMultiplier(multiplier) {
+  return function (num) {
+     return num * multiplier;
+   };
+ }
